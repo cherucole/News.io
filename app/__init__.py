15c0b111fe79aaa28cc85a88558eff1814e18a34
cherucole <cheruyotcheruyot@gmail.com>
2018-08-30 12:38:48 +0300

    failed to use macros on index page but everything works

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from .config import DevConfig
+from flask_bootstrap import Bootstrap
+
 
 #initializing the application
 app = Flask(__name__,instance_relative_config=True)
@@ -8,6 +10,9 @@ app = Flask(__name__,instance_relative_config=True)
 #Setting up the configuration for testing and production
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
+
+bootstrap = Bootstrap(app)
+
 
 
 from app import views
