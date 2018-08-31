4c78ef52a4bd8cc30519e8f3d36e36d7e7305835
cherucole <cheruyotcheruyot@gmail.com>
2018-08-31 15:55:23 +0300

    working categories

--- app/views.py
+++ app/views.py
@@ -26,16 +26,17 @@ def articles(id):
     # title=f'{articles.title}'
     return render_template('articles.html', articles=articles, name=name,name_source=id)
 
-@app.route('/categories/<category>')
-def categories(category):
+@app.route('/categories/<category_name>')
+def categories(category_name):
 
     '''
     View  page function that returns the details page and its data
     '''
     cat="cherucole"
-    category=get_category(category)
-    print(category)
+    category=get_category(category_name)
+    print(category_name)
+    category_name1=category_name
     # articles=get_articles(id)
     # print(articles)
     # title=f'{articles.title}'
-    return render_template('categories.html', category=category,name_source=id)
+    return render_template('categories.html', category=category,name_source=id,category_name1=category_name1)
